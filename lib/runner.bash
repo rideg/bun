90dc3358b69d145c4f2a1847551e21773e81ce9e
rideg <sandor.rideg@gmail.com>
2017-09-11 08:10:49 -0700

    description

--- lib/runner.bash
+++ lib/runner.bash
@@ -2,7 +2,13 @@
 
 bun::runner::run ()
 {
-  :
+  # parse arguments
+  # collect files 
+  # go trough locations (separate files from directories) 
+  # go trough directories and collect *test.sh & *test.bash files
+  # if recursive and find directories add them back
+  # repeat
+  # run each file one by one
 }
 
 bun::runner::run_single_file ()
